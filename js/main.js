2fb3ac64ec8c9d94e3db97bd143e15cf8aac8de4
homakimi <homakimi@gmail.com>
2024-03-12 19:04:07 +0800

    temp

--- js/main.js
+++ js/main.js
@@ -1,54 +1,27 @@
 $(function() {
-    scrollEffect();
-    $(window).scroll(function() {
-        scrollEffect();
-    })
-
-    // detect device change start
-    var isMobile, isMobileFirst, widthPC;
-    if(window.innerWidth > 1024) {
-        widthPC = true;
-    } else {
-        widthPC = false;
-    }
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        isMobileFirst = true;
-    } else {
-        isMobileFirst = false;
-    }
-    $(window).resize(function() {
-        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-            isMobile = true;
-        } else {
-            isMobile = false;
-        }
-        if(isMobileFirst != isMobile) {
-            location.reload();
-        }
-        if(widthPC && window.innerWidth <= 1024) {
-            location.reload();
-        } else if(!widthPC && window.innerWidth > 1024) {
-            location.reload();
-        }
-    })
-    // detect device change end
-
-
+    // scrollEffect();
+    // $(window).scroll(function() {
+    //     scrollEffect();
+    // })
+    $('.tab-wrap a:first-child').addClass('active');
+    $('.swiper .swiper-slide:first-child').addClass('active');
+    $('.content-block:not(:first-child)').hide();
+    $('.content-content:not(:first-child)').hide();
 })
 
-function scrollEffect() {
-    if($(window).scrollTop() > $('.content-2').offset().top - window.innerHeight*0.75) {
-        $('.lantern-left, .lantern-right').addClass('active');
-    }
-    $('.bam-newyear-title-ani').each(function() {
-        if($(window).scrollTop() > $(this).offset().top - window.innerHeight*0.75 ) {
-            $(this).addClass('active')
-        }
-    })
-    if($(window).scrollTop() > $('.game-wrap').offset().top - window.innerHeight*0.75) {
-        $('.game-intro-content').addClass('active')
-    }
-}
+// function scrollEffect() {
+//     if($(window).scrollTop() > $('.content-2').offset().top - window.innerHeight*0.75) {
+//         $('.lantern-left, .lantern-right').addClass('active');
+//     }
+//     $('.bam-newyear-title-ani').each(function() {
+//         if($(window).scrollTop() > $(this).offset().top - window.innerHeight*0.75 ) {
+//             $(this).addClass('active')
+//         }
+//     })
+//     if($(window).scrollTop() > $('.game-wrap').offset().top - window.innerHeight*0.75) {
+//         $('.game-intro-content').addClass('active')
+//     }
+// }
 $(document)
 .on('click', 'a', function(e) {
     if($(this).attr('href').length == 0) {
@@ -59,19 +32,21 @@ $(document)
 .on('mousedown', 'img', function(e) {
     e.preventDefault();
 })
-.on('click', '.notice-more', function() {
-    $(this).hide();
-    $('.notice-content').addClass('active');
+.on('click', '.tab-wrap a', function() {
+    if(!$(this).hasClass('active')) {
+        $('.tab-wrap a').removeClass('active');
+        $(this).addClass('active');
+        $('.tab-wrap').removeClass('active-0 active-1 active-2 active-3')
+        $('.tab-wrap').addClass('active-'+$(this).index());
+        $('.content-block').hide()
+        $('.content-block').eq($(this).index()).show();
+    }
 })
-.on('click', '.banner-bag', function() {
-    // random
-    var _random = parseInt(Math.random()*3)+1;
-    $('.bam-newyear-paper').css('background', 'url("img/bam-warm-bam-newyear-paper-'+_random+'.jpg")')
-    $('body').addClass('fix');
-    $('.bam-newyear-lightbox').fadeIn();
-    setTimeout(function() {
-        $('body').removeClass('fix');
-        $('.bam-newyear-lightbox').fadeOut();
-        $('body, html').animate({scrollTop: $('.content-1').offset().top - $('nav').height() }, 1000)
-    }, 5000)
+.on('click', '.swiper .swiper-slide', function() {
+    if(!$(this).hasClass('active')) {
+        $(this).closest('.swiper').find('.swiper-slide').removeClass('active');
+        $(this).addClass('active');
+        $(this).closest('.content-info').find('.content-content').hide();
+        $(this).closest('.content-info').find('.content-content').eq($(this).index()).show();
+    }
 })
